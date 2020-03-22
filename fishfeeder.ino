4b540cf494fa3ea91fb758ae0e8ef78ed853b7e5
michaeldvr <michael.devara@gmail.com>
2020-03-22 14:37:10 +0700

    status led

--- fishfeeder.ino
+++ fishfeeder.ino
@@ -1,30 +1,61 @@
+/* Fish Feeder
+   michaeldvr
+   version 1.0
+   ref:
+   - https://randomnerdtutorials.com/esp8266-dht11dht22-temperature-and-humidity-web-server-with-arduino-ide/
+   - https://randomnerdtutorials.com/esp8266-nodemcu-access-point-ap-web-server/
+   - https://github.com/maniacbug/StandardCplusplus
+*/
 #include <Wire.h>
 #include <RtcDS3231.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <Hash.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <Servo.h>
 
 RtcDS3231<TwoWire> Rtc(Wire);
-ESP8266WebServer server(80);
 
-const char *ssid = "TOKO 29";
+AsyncWebServer server(80);
+const char *ssid = "Fish Feeder";
 const char *password = "29situbondo";
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = D6; // the number of the pushbutton pin
-const int ledPin = D5;    // the number of the LED pin
+const uint8_t buttonPin = D7; // the number of the pushbutton pin
+const uint8_t statusLedPin = D5;    // the number of the LED pin
+const uint8_t temtPin = A0;
+const uint8_t foodLedPin = D6; // food level
 int switchValue;
+uint8_t counter = 0;
 
 Servo servo;
-const int servoPin = D8;
-const int minimumState = 0;
-const int maximumState = 180;
-int servoState = minimumState;
+const uint8_t servoPin = D8;
+const uint8_t TRAY_CLOSE = 0;
+const uint8_t TRAY_OPEN = 180;
+const uint8_t FEED_SIZE = 20; // ms
+const int FEEDING_DELAY = 600; // ms
+const float EMPTY_FOOD_THRESHOLD = 30;
+uint8_t servoState = TRAY_CLOSE;
+int feedingState = 0;
+uint8_t statusLedState = LOW;
+bool lowContent = false;
 
-void handle_index()
-{
-  server.send(200, "text/plain", "This is an index page.");
-}
+/* autofeeding schedule */
+struct schedule {
+  uint8_t h;
+  uint8_t m;
+  uint8_t s;
+};
+schedule feeding_schedule[] = {
+  {7, 0, 0},
+  {12, 0, 0},
+  {17, 30, 0},
+  {14, 6, 0},
+  {14, 6, 30},
+  {14, 7, 30}
+};
+const uint8_t SCHEDULE_SIZE = 6; // update when modify `feeding_schedule`
+int feedingInterval = 0;
 
 void setup()
 {
@@ -38,38 +69,38 @@ void setup()
     Rtc.SetDateTime(compiled);
   }
 
-  /*RtcDateTime now = Rtc.GetDateTime();*/
 
   Rtc.Enable32kHzPin(false);
   Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
 
-  WiFi.begin(ssid, password); //Connect to the WiFi network
+  WiFi.softAP(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED)
-  { //Wait for connection
-    delay(500);
-    Serial.println("Waiting to connect...");
-  }
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
 
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP()); //Print the local IP
+  /*Serial.print("IP address: ");
+    // Print ESP8266 Local IP Address
+    Serial.println(WiFi.localIP());*/
 
-  server.on("/", handle_index);
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/plain", "This is an index page");
+  });
 
   server.begin(); //Start the server
 
   pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT);
+  pinMode(statusLedPin, OUTPUT);
+  pinMode(temtPin, INPUT);
+  pinMode(foodLedPin, OUTPUT);
 
   // Servo
   servo.attach(servoPin);
   servo.write(servoState);
 }
 
-void loop()
-{
-  server.handleClient(); //Handling of incoming requests
-  RtcDateTime now = Rtc.GetDateTime();
+void print_date(RtcDateTime now, bool newline = false) {
+  if (counter % 10 != 0) return;
   //Print RTC time to Serial Monitor
   Serial.print("Date: ");
   Serial.print(now.Year(), DEC);
@@ -83,21 +114,88 @@ void loop()
   Serial.print(now.Minute(), DEC);
   Serial.print(':');
   Serial.print(now.Second(), DEC);
-  Serial.println();
+  if (newline) Serial.println();
+}
 
-  delay(1000); // one second
+void check_feeding_time(RtcDateTime now) {
+  if (feedingInterval == 0) {
+    bool found = false;
+    for (uint8_t i = 0; i < SCHEDULE_SIZE; i++) {
+      if (now.Hour() == feeding_schedule[i].h &&
+          now.Minute() == feeding_schedule[i].m &&
+          now.Second() == feeding_schedule[i].s) {
+        feedingInterval = FEEDING_DELAY;
+        feedingState = FEED_SIZE;
+        Serial.println("found schedule");
+      }
+    }
+  }
+  else {
+    feedingInterval--;
+  }
+}
+
+void start_feeding() {
+  if (feedingState > 0) {
+    if (servoState == TRAY_CLOSE) {
+      servoState = TRAY_OPEN;
+      servo.write(servoState);
+    }
+    if (statusLedState == LOW) {
+      statusLedState = HIGH;
+    }
+    else {
+      statusLedState = LOW;
+    }
+    digitalWrite(statusLedPin, statusLedState);
+    feedingState--;
+  }
+  else {
+    if (servoState == TRAY_OPEN) {
+      servoState = TRAY_CLOSE;
+      servo.write(servoState);
+    }
+    if (statusLedState == HIGH) {
+      statusLedState = LOW;
+      digitalWrite(statusLedPin, statusLedState);
+    }
+  }
+}
+
+void check_light() {
+  if (counter % 10 == 1) {
+    float reading = analogRead(temtPin);
+    Serial.print("intensity: ");
+    Serial.println(reading);
+    lowContent = (reading < EMPTY_FOOD_THRESHOLD);
+    digitalWrite(foodLedPin, lowContent);
+  }
+}
+
+void loop()
+{
+  // server.handleClient(); //Handling of incoming requests
+  RtcDateTime now = Rtc.GetDateTime();
+  print_date(now, true);
+
+  delay(100); // one second
   switchValue = digitalRead(buttonPin);
-  digitalWrite(ledPin, switchValue);
-  Serial.print("Button: ");
-  Serial.println(switchValue);
-  if (switchValue == HIGH && servoState == minimumState)
-  {
-    servoState = maximumState;
-    servo.write(servoState);
+  // digitalWrite(statusLedPin, switchValue);
+  // Serial.print(" | Button: ");
+  // Serial.println(switchValue);
+  if ( switchValue == HIGH || feedingState != 0) {
+    Serial.print("Button: ");
+    Serial.print(switchValue);
+    Serial.print(" | feedingState: ");
+    Serial.print(feedingState);
+    Serial.println();
   }
-  else if (switchValue == LOW && servoState == maximumState)
+  if (switchValue == HIGH && feedingState == 0)
   {
-    servoState = minimumState;
-    servo.write(servoState);
+    feedingState = FEED_SIZE;
   }
+  check_feeding_time(now);
+  start_feeding();
+  check_light();
+  counter++;
 }
